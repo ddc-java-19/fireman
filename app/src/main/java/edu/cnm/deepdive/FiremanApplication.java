11f832fb31c76de241b76ef1aacabcdc887b7af2
Chris <cgonzalez83@cnm.edu>
2024-11-11 10:20:48 -0700

    Changed FiremanApplication location

--- app/src/main/java/edu/cnm/deepdive/FiremanApplication.java
+++ app/src/main/java/edu/cnm/deepdive/FiremanApplication.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package edu.cnm.deepdive.fireman;
+package edu.cnm.deepdive;
 
 import android.app.Application;
 import dagger.hilt.android.HiltAndroidApp;
